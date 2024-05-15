a05fc0d9c3a0be3fc182e9bf921f8b8e62ef9f8f
Kenia <keniayhdiaz@gmail.com>
2024-05-15 00:58:06 -0500

    feat:

--- js/modules/rockets.js
+++ js/modules/rockets.js
@@ -1,3 +1,5 @@
+
+
 function fetchRockets(index) {
     // Agrega la clase de animación de carga al elemento del encabezado cuando se inicia la solicitud a la API
     document.getElementById("headerRockets").classList.add("loding");
@@ -35,18 +37,24 @@ function displayHeaderRockets(rocket) {
     var valor = (rocket.cost_per_launch)
     var numeroFormateado = "$ " + new Intl.NumberFormat('es-ES').format(valor);
 
+    var kilo = (rocket.mass.kg)
+    var numeroKilo = new Intl.NumberFormat('es-ES').format(kilo) + " kg";
+
+    var libra = (rocket.mass.lb)
+    var numeroLibra = new Intl.NumberFormat('es-ES').format(libra) + " lb";
+
     let generalInfo = document.getElementById("headerRockets");
     generalInfo.innerHTML = `
         <p id="namee">${rocket.name}</p>
     `;
     let imageRocket = document.getElementById("image");
     imageRocket.innerHTML = `
-        <img src="${rocket.flickr_images[0]}" alt="imagen1" class="imgNave">
-        <img src="${rocket.flickr_images[1]}" alt="imagen1" class="imgNave">
-        <img src="${rocket.flickr_images[2]}" alt="imagen1" class="imgNave">
-        <img src="${rocket.flickr_images[3]}" alt="imagen1" class="imgNave">
-        <img src="${rocket.flickr_images[4]}" alt="imagen1" class="imgNave">
-        <img src="${rocket.flickr_images[5]}" alt="imagen1" class="imgNave">
+        <img src="${rocket.flickr_images[0]}" alt="imagen1" class="imgNave" referrerpolicy="no-referrer">
+        <img src="${rocket.flickr_images[1]}" alt="imagen1" class="imgNave" referrerpolicy="no-referrer">
+        <img src="${rocket.flickr_images[2]}" alt="imagen1" class="imgNave" referrerpolicy="no-referrer">
+        <img src="${rocket.flickr_images[3]}" alt="imagen1" class="imgNave" referrerpolicy="no-referrer">
+        <img src="${rocket.flickr_images[4]}" alt="imagen1" class="imgNave" referrerpolicy="no-referrer">
+        <img src="${rocket.flickr_images[5]}" alt="imagen1" class="imgNave" referrerpolicy="no-referrer">
         
     `;
     let infoRocket = document.getElementById("info1");
@@ -71,7 +79,7 @@ function displayHeaderRockets(rocket) {
         <h2>Read more about the coete</h2>
         <p><a href="${rocket.wikipedia}" id="wiki">Wikipedia</a></p>
     `;
-    let moreInfo = document.getElementById("moreInfo")
+    let moreInfo = document.getElementById("moreInfo");
     moreInfo.innerHTML = `
         <div class="infoMore" id="more1">
             <span id="spam">Type</span>
@@ -98,7 +106,7 @@ function displayHeaderRockets(rocket) {
             <strong id="strom">${rocket.landing_legs.material}</strong>
         </div>
     `;
-    let moreInfoo = document.getElementById("moreInfoo")
+    let moreInfoo = document.getElementById("moreInfoo");
     moreInfoo.innerHTML = `
         <div class="infoMore" id="more1">
             <span id="spam">Type</span>
@@ -125,4 +133,90 @@ function displayHeaderRockets(rocket) {
             <strong id="strom">${rocket.engines.propellant_2}</strong>
         </div>
     `;
+ 
+
+    let informationNave = document.getElementById("infoRocket");
+    informationNave.innerHTML = `
+    <div class="informationContainer" id="informationNave1">
+        <div class="progress-container">
+            <label id="spam">Rocket diameter : </label>
+            <progress max="13.1" value="${rocket.diameter.meters}"></progress>
+        </div>
+        <div class="div2">
+            <span id="spam">${(rocket.diameter.meters).toString().replace(".", ",") + "     M"}<br>${(rocket.diameter.feet).toString().replace(".", ",") + "     F"}</span>
+        </div>
+    </div>
+    <div class="informationContainer" id="informationNave2">
+        <div class="progress-container">
+            <label id="spam">Rocket weight : </label>
+            <progress max="1420788" value="${rocket.mass.kg}"></progress>
+        </div>
+        <div class="div2">
+            <span id="spam">${numeroKilo}<br>${numeroLibra}</span>
+        </div>
+    </div>
+    <div class="informationContainer" id="informationNave7">
+        <div class="progress-container">
+            <label id="spam">Rocket height : </label>
+            <progress max="118" value="${rocket.height.meters}"></progress>
+        </div>
+        <div class="div2">
+            <span id="spam"><span id="spam">${(rocket.height.meters).toString().replace(".", ",") + "     M"}<br>${(rocket.height.feet).toString().replace(".", ",") + "     F"}</span></span>
+        </div>
+    </div>
+    <div class="informationContainer" id="informationNave7">
+        <div class="progress-container">
+            <label id="spam">Diameter rocket shield : </label>
+            <progress max="5.2" value="${(rocket.second_stage.payloads.composite_fairing.diameter.meters)}"></progress>
+        </div>
+        <div class="div2">
+            <span id="spam"><span id="spam">${(rocket.second_stage.payloads.composite_fairing.diameter.meters).toString().replace(".", ",") + "     M"}<br>${(rocket.second_stage.payloads.composite_fairing.diameter.feet).toString().replace(".", ",") + "     F"}</span></span>
+        </div>
+    </div>
+    <div class="informationContainer" id="informationNave7">
+        <div class="progress-container">
+            <label id="spam">Height rocket shield : </label>
+            <progress max="13.1" value="${(rocket.second_stage.payloads.composite_fairing.height.meters)}"></progress>
+        </div>
+        <div class="div2">
+            <span id="spam"><span id="spam">${(rocket.second_stage.payloads.composite_fairing.height.meters).toString().replace(".", ",") + "     M"}<br>${(rocket.second_stage.payloads.composite_fairing.height.feet).toString().replace(".", ",") + "     F"}</span></span>
+        </div>
+    </div>
+    `;
+    rocket.payload_weights.forEach(function(weight) {
+        // Crear un nuevo div para cada grupo de pesos de carga
+        var weightDiv = document.createElement("div");
+        weightDiv.classList.add("informationContainer");
+        
+        // Generar la barra de progreso para el peso actual
+        var progressBar = generateProgressBar(weight.kg, 150000); // Usar weight.kg como value y 1500000 como max
+        
+        // HTML interno del div
+        weightDiv.innerHTML = `
+            <div class="progress-container">
+                <label id="spam">${weight.name} : </label>
+                ${progressBar.outerHTML} <!-- Insertar la barra de progreso generada -->
+            </div>
+            <div class="div2">
+                <span id="spam">${(weight.kg.toLocaleString('es-ES')) + " kg"}<br>${(weight.lb.toLocaleString('es-ES')) + " lb"}</span>
+            </div>
+        `;
+    
+        // Agregar el div al contenedor principal
+        informationNave.appendChild(weightDiv);
+    });
+
+
+}
+
+function generateProgressBar(value, max) {
+    const progress = document.createElement("progress");
+    progress.max = max;
+    progress.value = value;
+
+    const container = document.createElement("div");
+    container.classList.add("progress-container");
+    container.appendChild(progress);
+
+    return container;
 }
